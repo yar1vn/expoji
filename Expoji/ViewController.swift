540160d9bb59a7cefa2a2b18ec3b3443af00c916
Yariv <yariv.nis@icloud.com>
2017-06-16 16:52:31 -0700

    Disable auto layout and improve the random point generator

--- Expoji/ViewController.swift
+++ Expoji/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  Expoji
+//  Exploji
 //
 //  Created by Yariv on 6/16/17.
 //  Copyright © 2017 Yariv. All rights reserved.
@@ -9,17 +9,67 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet private weak var button: UIButton!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction private func launch(_ sender: Any) {
+        fadeOut {
+            self.exploji()
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        button.center = view.center
     }
-
-
+    
+    private var emojiViews: [UIView] = []
+    
+    private func fadeOut(completion: @escaping (Void) -> ()) {
+        if emojiViews.isEmpty {
+            completion()
+            return
+        }
+        let views = emojiViews
+        emojiViews = []
+        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 1, delay: 0, options: [.allowUserInteraction, .beginFromCurrentState], animations: {
+            views.forEach { $0.alpha = 0 }
+        }) { (_) in
+            views.forEach { $0.removeFromSuperview() }
+        }
+        completion()
+    }
+    
+    private func exploji() {
+        for _ in 1...12 {
+            let emoji = self.createEmoji()
+            self.emojiViews.append(emoji)
+        }
+        
+        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 5, delay: 0, options: [.allowUserInteraction, .beginFromCurrentState], animations: {
+            self.emojiViews.forEach {
+                $0.center = self.randomPoint()
+            }
+        }, completion: nil)
+    }
+    
+    private func randomPoint() -> CGPoint {
+        let angle = CGFloat(arc4random_uniform(360))
+        let x = cos(angle) * view.frame.width*2
+        let y = sin(angle) * view.frame.height*2
+        return CGPoint(x: x, y: y)
+    }
+    
+    private func createEmoji() -> UIView {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        let emojis = ["👮","👨‍🏫","🐹","🐷","🐽","🐛","🐃","🦂","🕸","🐙","🦍","🌊","⚡️","🌔","🌩","🍚","🌽","🥐","🍣","🍦","🍵","🍩","🍫"]
+        let index = Int(arc4random_uniform(UInt32(emojis.count)))
+        label.text = emojis[index]
+        label.font = UIFont.systemFont(ofSize: 40)
+        label.sizeToFit()
+        view.addSubview(label)
+        label.center = view.center
+        return label
+    }
 }
 
